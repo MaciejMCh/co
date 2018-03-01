63340ccae05be6c2ec724d8201e93f12b28bef25
Maciej Chmielewski <maciej.chmielewski@appunite.com>
2018-03-02 00:05:54 +0100

    downloading

--- main.swift
+++ main.swift
@@ -3,8 +3,35 @@ import Foundation
 var lastTickTimestamp = Date().timeIntervalSince1970
 let interval: Double = 2
 
+func request(url: String, completion: @escaping (Data?) -> Void) {
+    DispatchQueue.global().async {
+        guard let url = URL(string: url) else {
+            completion(nil)
+            return
+        }
+        guard let data = try? Data(contentsOf: url) else {
+            completion(nil)
+            return
+        }
+        completion(data)
+    }
+}
+
+func write(data: Data, name: String) {
+    let path = "/Users/maciejchmielewski/Desktop/data/" + name
+    try? data.write(to: URL(fileURLWithPath: path))
+    print(path)
+}
+
+var lastData = "none".data(using: .utf8)!
 while true {
     while Date().timeIntervalSince1970 < lastTickTimestamp + interval {}
     lastTickTimestamp = Date().timeIntervalSince1970
-    print("xd")
+    request(url: "https://google.com") {
+        write(data: $0 ?? lastData, name: String(Int(lastTickTimestamp)))
+        if let data = $0 {
+            lastData = data
+        }
+    }
+    
 }
