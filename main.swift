e5d1eeed9a99fc2a649bdb72ee771838590da670
Maciej Chmielewski <maciej.chmielewski@appunite.com>
2018-03-01 23:43:45 +0100

    interval tick

--- main.swift
+++ main.swift
@@ -1,1 +1,10 @@
-print("xd")
+import Foundation
+
+var lastTickTimestamp = Date().timeIntervalSince1970
+let interval: Double = 2
+
+while true {
+    while Date().timeIntervalSince1970 < lastTickTimestamp + interval {}
+    lastTickTimestamp = Date().timeIntervalSince1970
+    print("xd")
+}
